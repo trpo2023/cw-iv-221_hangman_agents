7482db020a0b92b70a92148e318da34fb4f0c07f
Andres-LaMa <andrejershov5045@gmail.com>
2023-05-21 17:30:24 +0700

    creating a global header file for convenient control of plug-in header files and libraries #9

---
+++ data/src/global.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <climits>
+#include <string.h>
+
+extern int music;
+extern int volume;
+
+void free_stdin();
