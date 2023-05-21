4409006dbfbcab36360c577c4659a159c372ca60
Egor_Gretskiy <gretskiy.egor@mail.ru>
2023-05-21 22:56:12 +0700

    Create get dynamic path

---
+++ data/src/library/graphics/path.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+
+std::string get_path(std::string str, std::string asset)
+{
+    std::string temp;
+    str = get_current_dir_name();
+    int slash = 0;
+    int len = 0;
+    while (str[len] != '\0')
+    {
+        if (str[len] == '/')
+        {
+            slash++;
+        }
+        if (slash == 4)
+        {
+            break;
+        }
+        len++;
+    }
+    temp = str.substr(0, len + 1) + "assets/" + asset;
+    return temp;
+}
