fadc7b98a500be1e7ef079261e9bfc75db4aec89
Egor_Gretskiy <gretskiy.egor@mail.ru>
2023-05-21 18:19:25 +0700

    Modify structure project

---
+++ data/src/library/graphics/button.hpp
@@ -0,0 +1,20 @@
+#include "globals.hpp"
+#include "mouse.hpp"
+
+#pragma once
+
+class Button {
+public:
+    SDL_Texture* tex;
+    SDL_Rect srect, drect;
+    std::string asset;
+    bool isSelected = false;
+    SDL_Surface* s;
+    SDL_Texture* t;
+
+    Button();
+    // virtual ~Button();
+    void draw();
+    void get_texture();
+    void update(Mouse& mouse);
+};
---
+++ data/src/library/graphics/globals.hpp
@@ -0,0 +1,7 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+#include <wordexp.h>
+
+extern SDL_Window* sWindow;
+extern SDL_Renderer* sRenderer;
