9dfbc88349c6f003dff71371ff9d14cc6a6de667
Egor_Gretskiy <gretskiy.egor@mail.ru>
2023-05-23 15:10:08 +0700

    Add value, chosed condition, asset for buttons #4

---
+++ data/src/library/button.hpp
@@ -0,0 +1,22 @@
+#include "globals.hpp"
+#include "mouse.hpp"
+
+#pragma once
+
+class Button {
+public:
+    SDL_Texture* tex;
+    SDL_Rect srect, drect;
+    std::string asset;
+    bool isSelected = false;
+    SDL_Surface* s;
+    SDL_Texture* t;
+    wchar_t value;
+    bool chosed;
+
+    Button();
+    // virtual ~Button();
+    void draw();
+    void get_texture();
+    void update(Mouse& mouse);
+};
